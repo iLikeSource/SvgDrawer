bc38996f47d93772f861cea3f6823c7fe7f287c5
iLikeSource <soszk505u2uk1@gmail.com>
2017-09-18 14:14:18 +0900

    RC断面の描画を実装

---
+++ KenchikuThinBook/RCSection.fsx
@@ -0,0 +1,34 @@
+﻿
+
+#I @"bin\Debug\"
+#r "SvgDrawer.dll"
+#r "KenchikuThinBook.dll"
+#r "WindowsBase.dll"
+#r "PresentationCore.dll"
+#load "RCSectionHelper.fs"
+
+open System
+open System.Windows
+open SvgDrawer
+open KTB
+open RCSectionHelper
+
+let model : SvgDrawer.Model = 
+    { BlockSize   = 10.0 
+      RowCount    = 30 
+      ColumnCount = 30
+      Position    = (0, 0) 
+      StrokeColor = Color.Black
+      StrokeWidth = 2.0
+      Shapes      = [] }
+
+let section = 
+    { Position    = (10, 10) 
+      CountOfBarX = 4 
+      CountOfBarY = 4 }
+
+let draw () = 
+    section.Draw (model)
+    |> SvgHelper.save @"C:\Users\So\Documents\Programs\other\kenchiku-thin-book\kenchiku-thin-book\md\html\image\RCSection.bmp"
+
+draw ()
---
+++ KenchikuThinBook/RCSectionHelper.fs
@@ -0,0 +1,56 @@
+﻿module RCSectionHelper
+
+open System.Windows
+open SvgDrawer
+open KTB
+
+
+type RCSection = 
+    { Position           : int * int
+      CountOfBarX        : int 
+      CountOfBarY        : int }
+    with
+    member private this.BarCoordinates (ox : int, 
+                                        oy : int, 
+                                        width : int, 
+                                        height : int) = 
+
+        [
+            //  左上から左下
+            [ 1 .. this.CountOfBarX ] |> List.map (fun y -> (2, y * 2))
+            //  左下から右下
+            [ 1 .. this.CountOfBarY ] |> List.map (fun x -> (x * 2, this.CountOfBarX * 2))
+            //  右上から右下
+            [ 1 .. this.CountOfBarX ] |> List.map (fun y -> (this.CountOfBarY * 2, y * 2))
+            //  左上から右上
+            [ 1 .. this.CountOfBarY ] |> List.map (fun x -> (x * 2, 2))
+        ]
+        |> List.concat
+        |> List.map (fun (x, y) -> (x + (ox - width / 2), y + (oy - height / 2)))
+             
+    member this.Draw (model : SvgDrawer.Model) = 
+        let drawRebarRectangle barCoordinates model =
+            List.fold (fun model (x, y) ->
+                model
+                |> Action.Move(x, y).On
+                |> Circle.On
+                |> Attr.Radius(0.2).With 
+                |> Attr.StrokeColor("black").With
+            ) model barCoordinates
+        
+        let getSize (x) = (x + 1) * 2
+        
+        let height =  getSize this.CountOfBarX
+        let width  =  getSize this.CountOfBarY
+
+        let (ox, oy) = this.Position
+        let barCoordinates = this.BarCoordinates (ox, oy, width, height)
+        
+        model
+        |> Action.Move(ox, oy).On
+        |> Rectangle.On
+        |> Attr.Width(float width).With    
+        |> Attr.Height(float height).With 
+        |> drawRebarRectangle barCoordinates
+                
+                    
